f081dbd06e2590dc696e1be9d9b1f315404d86e0
steven <steven@peipeiyun.com>
2017-10-17 16:40:35 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld fromfather="send to child" />
+    <HelloWorld :fromfather="sendToChild" />
+    <ChildToFather @listenChildChange="showChildChange" />
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ChildToFather from './components/ChildToFather'
+export default {
+  name: 'app',
+  components:{
+  	HelloWorld,
+  	ChildToFather
+  },
+  data(){
+  	return {
+  		sendToChild:"变量 改变"
+  	}
+  },
+  methods:{
+  	showChildChange:function(data){
+  		console.log(data)
+  	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ChildToFather.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h2>子级给父级传值测试</h2>
+		<button @click="sendMsg">子级给父级传值</button>
+	</div>
+</template>
+
+<script>
+	
+	export default{
+		methods:{
+			sendMsg:function(){
+				this.$emit("listenChildChange","this msg from child")
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
